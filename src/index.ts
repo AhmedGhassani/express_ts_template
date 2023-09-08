5eb8e8b76cfc769260cc413c2d27a1262ad6df62
Ahmed Al Ghassani <ahmed-ghassani@outlook.com>
2023-09-08 15:24:53 +0400

    Reconfigure eslint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("App is Healthy")
-})
+  res.status(200).send("App is Healthy");
+});
 
 app.listen(port, () => {
-  console.log(`Server is listening on ${port}`)
-  console.log(`Health Check: http://localhost:${port}`)
-})
+  console.log(`Server is listening on ${port}`);
+  console.log(`Health Check: http://localhost:${port}`);
+});
