0e895327e5bed9b838cdcb7e607ee43eafeee460
Ahmed Al Ghassani <ahmed-ghassani@outlook.com>
2023-09-08 14:40:10 +0400

    Configure linter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, {Request, Response} from 'express';
+import express, {Request, Response} from "express";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('App is Healthy')
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("App is Healthy")
 })
 
 app.listen(port, () => {
